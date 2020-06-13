17c82f50812e442ae39050d18b8a4cd5001711a9
fprandi8 <fprandi@est.frba.utn.edu.ar>
2020-06-12 22:52:13 -0300

    Cambios varios, tiene error de SIGABRT

--- Broker/src/Broker.h
+++ Broker/src/Broker.h
@@ -33,13 +33,21 @@
 #include "delibird/comms/messages.h"
 #include "delibird/comms/serialization.h"
 #include "delibird/comms/pokeio.h"
+#include <signal.h>
 
 
 typedef struct {
 	message_type type;
 	t_queue* queue;
 	t_list* suscriptors;
+	t_list* messagesAdministrator;
 	} t_queue_handler;
+
+typedef struct{
+	uint32_t messageId;
+	uint32_t amountPendingAcknowledge;
+	sem_t semaphoreACK;
+	}t_message_administrator;
 
 typedef struct {
 	int suscripted;
@@ -49,6 +57,8 @@ typedef struct {
 	t_list * queues;
 	t_CacheMemory* cacheMemory;
 	sem_t semaphoreID;
+	int servidor;
+	t_log * logger;
 	} t_Broker;
 
 typedef struct {
@@ -56,23 +66,31 @@ typedef struct {
 	t_Broker * broker;
 	} t_args;
 
+typedef struct {
+	t_queue_handler* queue;
+	deli_message* message;
+	t_Broker* broker;
+	int cliente;
+	t_message_administrator* messageAdministrator;
+	} t_args_queue;
 
 
 t_log* iniciar_logger(void);
 t_config* leer_config(void);
 char* obtener_valor_config(t_config* , t_log* , char* );
 
-void Broker_initialize(t_Broker*);
-void Broker_destroy(t_Broker*);
-void Broker_Suscribe_Process(t_buffer* buffer, int cliente, t_Broker* broker);
-t_queue_handler* Broker_Get_Specific_Queue(t_Broker , message_type );
-void Broker_Get_Acknowledge(t_buffer* , int , t_Broker* );
-void Broker_Process_Message(t_buffer* , int , t_Broker* );
-void Broker_Assign_id(t_Broker* , deli_message* );
+void broker_initialize(t_Broker*, int, t_log*);
+void broker_destroy(t_Broker*);
+void broker_suscribe_process(void* buffer, int cliente, t_Broker* broker);
+t_queue_handler* broker_get_specific_Queue(t_Broker , message_type );
+void broker_get_acknowledge(void* , int , t_Broker* );
+void broker_process_message(void* , int , t_Broker* );
+void broker_assign_id(t_Broker* , deli_message* );
 
 
 t_queue_handler* queue_handler_initialize(message_type );
-void queue_handler_process_message(t_queue_handler* , deli_message*);
+void queue_handler_process_message(t_queue_handler* , deli_message*, t_Broker*);
+void queue_handler_send_message(void* );
 
 int destroy_queue_list(t_list*);
 void destroy_queue_handler(t_queue_handler* );
@@ -80,7 +98,15 @@ void destroy_queue_handler(t_queue_handler* );
 t_suscriptor* queue_handler_get_suscriptor(t_queue_handler* ,int );
 
 void serve_client(void *);
-void recive_message(t_package* , int, t_Broker*);
+void recive_message(uint32_t , int, t_Broker*, void*);
+
+t_message_administrator* message_administrator_initialize(uint32_t);
+t_message_administrator* messege_administrator_get_administrator(t_list* , uint32_t);
+void message_administrator_pending_acknowledge(t_message_administrator* );
+void message_administrator_receive_acknowledge(t_message_administrator* );
+uint32_t message_admnistrator(t_message_administrator*);
 
 
+
+void signaltHandler(int);
 #endif /* BROKER_H_ */

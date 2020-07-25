ccdc05f2ebd1cb2b262258841009f8445ece667c
fprandi8 <fprandi@est.frba.utn.edu.ar>
2020-07-25 16:52:18 -0300

    subo make

---
+++ Broker/Debug/src/Broker.d
@@ -0,0 +1,54 @@
+src/Broker.o: ../src/Broker.c ../src/Broker.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/log.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/queue.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/node.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h \
+ ../src/utils.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/config.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/dictionary.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h \
+ ../src/CacheMemory.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/memory.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/string.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/temporal.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/pokeio.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h
+
+../src/Broker.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/log.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/queue.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/node.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h:
+
+../src/utils.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/config.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/dictionary.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h:
+
+../src/CacheMemory.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/memory.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/string.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/temporal.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/pokeio.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h:
---
+++ Broker/Debug/src/CacheMemory.d
@@ -0,0 +1,45 @@
+src/CacheMemory.o: ../src/CacheMemory.c ../src/CacheMemory.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/log.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/queue.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/node.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/memory.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/string.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/temporal.h \
+ ../src/utils.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/config.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/dictionary.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h
+
+../src/CacheMemory.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/log.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/queue.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/node.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/list.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/memory.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/string.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/temporal.h:
+
+../src/utils.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/config.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/dictionary.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h:

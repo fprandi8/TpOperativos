2926e8d394ca48ffc16c6997c980c3874895eea5
fprandi8 <fprandi@est.frba.utn.edu.ar>
2020-07-25 17:12:17 -0300

    make del gameboy

---
+++ GameBoy/Debug/script_get.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+./GameBoy BROKER GET_POKEMON Pikachu
+./GameBoy BROKER GET_POKEMON Charmander
+./GameBoy BROKER GET_POKEMON Charmander
+./GameBoy BROKER GET_POKEMON Squrtel
+./GameBoy BROKER GET_POKEMON Pidgey
+
---
+++ GameBoy/Debug/src/GameBoy.d
@@ -0,0 +1,28 @@
+src/GameBoy.o: ../src/GameBoy.c \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/pokeio.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/string.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/config.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/dictionary.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/node.h \
+ /home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/log.h
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/pokeio.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/messages.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/delibird/comms/serialization.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/string.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/config.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/dictionary.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/collections/node.h:
+
+/home/utnso/workspace/tp-2020-1c-MATE-OS/SharedLibrary/commons/log.h:
